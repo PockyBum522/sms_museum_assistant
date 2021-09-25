7fbec4213d5f443b3820e89071b321eee2f7b676
DSikes <david@sikespc.com>
2021-09-25 16:21:18 -0400

    Passing phone number tested, setting up message response handler

--- index.js
+++ index.js
@@ -4,29 +4,51 @@ var telnyx = require('telnyx')('KEY017C1DE7B63E8ABC4ECB54DF31105EEC_49fQTrNX8GBm
 const app = express()
 const port = 3000
 
-const testPhoneNumber = '+14076322207';
-
 const telnyxApiBaseUrl = "https://api.telnyx.com/v2"
+
+const incomingWebhookUrlBase = 'http://pockybum522.com:3000`';
+const incomingWebhookEndpoint = 'onIncoming/session123';
+
+const assistantIntroText = "Welcome to The Orlando Museum of Art! I can be your assistant during your visit.\n\nAsk me anything from information about an artwork to assistance navigating in the museum.\n\nFor example, you can ask: 'Tell me more about Van Gogh's Starry Night' or 'Where is the nearest water fountain?'\n\nAfter your visit, ask me about the best way to leave a review or feedback!";
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
-app.get('/startListener:phoneNumber', (req, res) => {
+app.get('/startListener/:phoneNumber', (req, res) => {
 
-  telnyx.messages.create(
-      {
-        'from': '+12182203711', // Your Telnyx number
-        'to': testPhoneNumber,
-        'text': 'Hello, World!'
-      },
-      function(err, response) {
-        // asynchronously called
-        res.send(respo);
-      }
-  );
+    let formattedPhoneNumber = `+1${ req.params.phoneNumber }`;
+
+    Promise.resolve(
+        telnyx.messages.create(
+                {
+                    'from': '+12182203711', // Your Telnyx number
+                    'to': formattedPhoneNumber,
+                    'text': assistantIntroText,
+                    'webhook_url': `${ incomingWebhookUrlBase }/${ incomingWebhookEndpoint }"`
+                })
+        )
+        .then(() =>
+            app.on('event:incoming_message', onIncomingMessage)
+        )
+        .then(() =>
+            res.send(`Listener started and introduction message sent for: ${ formattedPhoneNumber }`))
+
+});
+
+app.get(`/${incomingWebhookEndpoint}`, (req, res) => {
+
+    // Categorize message
+
+    // Build response
+
+    // Send response
 
 })
+
+function onIncomingMessage(){
+
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
